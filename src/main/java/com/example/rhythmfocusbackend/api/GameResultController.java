c61da88755e14b964439297127f652ab151c4064
sinsohi <sohi8451@naver.com>
2025-11-18 06:07:06 +0900

    feat: GameResult 컨트롤러 구현

---
+++ src/main/java/com/example/rhythmfocusbackend/api/GameResultController.java
@@ -0,0 +1,47 @@
+package com.example.rhythmfocusbackend.api;
+
+import com.example.rhythmfocusbackend.domain.gameresult.dto.GameResultRequestDTO;
+import com.example.rhythmfocusbackend.domain.gameresult.dto.UserGameStats;
+import com.example.rhythmfocusbackend.domain.gameresult.entity.GameResultEntity;
+import com.example.rhythmfocusbackend.domain.gameresult.service.GameResultService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/game-result")
+@RequiredArgsConstructor
+public class GameResultController {
+
+    private final GameResultService gameResultService;
+
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Map<String, Long>> saveGameResult(
+            @Validated GameResultRequestDTO dto
+    ) {
+        Long id =  gameResultService.saveGameResult(dto);
+        return ResponseEntity.ok().body(Collections.singletonMap("id", id));
+    }
+
+    @GetMapping(value = "/list")
+    public ResponseEntity<List<GameResultEntity>> getGameResults() {
+        List<GameResultEntity> results = gameResultService.getUserGameResults();
+        return ResponseEntity.ok(results);
+    }
+
+    @GetMapping(value = "/stats")
+    public ResponseEntity<UserGameStats> getUserGameStats() {
+        UserGameStats stats = gameResultService.getUserStats();
+        return ResponseEntity.ok(stats);
+    }
+
+}
