79361f4abe3cdf2c918521dedfe689096de60502
cbnuLeehyunwoo <henow123@naver.com>
2025-11-23 14:04:58 +0900

    feat: CFQ, BDI-II를 채점 및 판독하는 기능 구현

--- src/main/java/com/example/rhythmfocusbackend/domain/diagnosis/entity/SurveyType.java
+++ src/main/java/com/example/rhythmfocusbackend/domain/diagnosis/entity/SurveyType.java
@@ -2,5 +2,7 @@ package com.example.rhythmfocusbackend.domain.diagnosis.entity;
 
 public enum SurveyType {
     ASRS,       // 성인 ADHD 척도
-    SNAP_IV     // 아동용 척도
+    SNAP_IV,     // 아동용 척도
+    CFQ,
+    BDI_II
 }
