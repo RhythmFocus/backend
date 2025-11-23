1655c7be94de7d7eac440df36a765039712d011f
cbnuLeehyunwoo <henow123@naver.com>
2025-11-23 14:03:11 +0900

    feat: Diagnosis Request에 Setter 추가

--- src/main/java/com/example/rhythmfocusbackend/domain/diagnosis/dto/DiagnosisRequest.java
+++ src/main/java/com/example/rhythmfocusbackend/domain/diagnosis/dto/DiagnosisRequest.java
@@ -1,13 +1,13 @@
 package com.example.rhythmfocusbackend.domain.diagnosis.dto;
 
-import lombok.Getter;
+import lombok.Data;
 import lombok.NoArgsConstructor;
 import java.util.Map;
 
-@Getter
+@Data
 @NoArgsConstructor
 public class DiagnosisRequest {
-    private String surveyType; // "ASRS" or "SNAP-IV"
+    private String surveyType; //
     private Map<String, Integer> answers; // 문항번호: 점수
     private String gender; // "male" or "female" (SNAP-IV용)
 }
